c167d06ec1c1dad82cfc530b82264f752bfea5cf
Yoon-Ki Hong <mymy49@nate.com>
2022-11-05 13:15:50 +0900

    [GD32F450Z-EVAL 보드 예제 업데이트] - SDRAM 활성화

--- [GD32F450ZK]GD32F450Z-EVAL/prj/src/bsp.cpp
+++ [GD32F450ZK]GD32F450Z-EVAL/prj/src/bsp.cpp
@@ -19,6 +19,7 @@
 #include <yss/debug.h>
 #include <yss.h>
 #include <bsp.h>
+#include <mod/sdram/MT48LC16M16A2_6A.h>
 
 FunctionQueue functionQueue(16);
 CommandLineInterface cli(uart2);
@@ -49,5 +50,60 @@ void initBoard(void)
 
 	// LED 초기화
 	led::init();
+}
+
+void initSdram(void)
+{
+	using namespace define::gpio::altfunc;
+
+	Gpio::AltFunc sdramPort[39]{
+		{(YSS_GPIO_Peri*)GPIOF, 0, PF0_FMC_A0},
+		{(YSS_GPIO_Peri*)GPIOF, 1, PF1_FMC_A1},
+		{(YSS_GPIO_Peri*)GPIOF, 2, PF2_FMC_A2},
+		{(YSS_GPIO_Peri*)GPIOF, 3, PF3_FMC_A3},
+		{(YSS_GPIO_Peri*)GPIOF, 4, PF4_FMC_A4},
+		{(YSS_GPIO_Peri*)GPIOF, 5, PF5_FMC_A5},
+		{(YSS_GPIO_Peri*)GPIOF, 12, PF12_FMC_A6},
+		{(YSS_GPIO_Peri*)GPIOF, 13, PF13_FMC_A7},
+		{(YSS_GPIO_Peri*)GPIOF, 14, PF14_FMC_A8},
+		{(YSS_GPIO_Peri*)GPIOF, 15, PF15_FMC_A9},
+		{(YSS_GPIO_Peri*)GPIOG, 0, PG0_FMC_A10},
+		{(YSS_GPIO_Peri*)GPIOG, 1, PG1_FMC_A11},
+		{(YSS_GPIO_Peri*)GPIOG, 2, PG2_FMC_A12},
+
+		{(YSS_GPIO_Peri*)GPIOG, 4, PG4_FMC_BA0},
+		{(YSS_GPIO_Peri*)GPIOG, 5, PG5_FMC_BA1},
+
+		{(YSS_GPIO_Peri*)GPIOD, 14, PD14_FMC_D0},
+		{(YSS_GPIO_Peri*)GPIOD, 15, PD15_FMC_D1},
+		{(YSS_GPIO_Peri*)GPIOD, 0, PD0_FMC_D2},
+		{(YSS_GPIO_Peri*)GPIOD, 1, PD1_FMC_D3},
+		{(YSS_GPIO_Peri*)GPIOE, 7, PE7_FMC_D4},
+		{(YSS_GPIO_Peri*)GPIOE, 8, PE8_FMC_D5},
+		{(YSS_GPIO_Peri*)GPIOE, 9, PE9_FMC_D6},
+		{(YSS_GPIO_Peri*)GPIOE, 10, PE10_FMC_D7},
+		{(YSS_GPIO_Peri*)GPIOE, 11, PE11_FMC_D8},
+		{(YSS_GPIO_Peri*)GPIOE, 12, PE12_FMC_D9},
+		{(YSS_GPIO_Peri*)GPIOE, 13, PE13_FMC_D10},
+		{(YSS_GPIO_Peri*)GPIOE, 14, PE14_FMC_D11},
+		{(YSS_GPIO_Peri*)GPIOE, 15, PE15_FMC_D12},
+		{(YSS_GPIO_Peri*)GPIOD, 8, PD8_FMC_D13},
+		{(YSS_GPIO_Peri*)GPIOD, 9, PD9_FMC_D14},
+		{(YSS_GPIO_Peri*)GPIOD, 10, PD10_FMC_D15},
+
+		{(YSS_GPIO_Peri*)GPIOE, 0, PE0_FMC_NBL0},
+		{(YSS_GPIO_Peri*)GPIOE, 1, PE1_FMC_NBL1},
+		{(YSS_GPIO_Peri*)GPIOG, 8, PG8_FMC_SDCLK},
+		{(YSS_GPIO_Peri*)GPIOC, 0, PC0_FMC_SDNWE},
+		{(YSS_GPIO_Peri*)GPIOF, 11, PF11_FMC_SDNRAS},
+		{(YSS_GPIO_Peri*)GPIOG, 15, PG15_FMC_SDNCAS},
+		{(YSS_GPIO_Peri*)GPIOC, 3, PC3_FMC_SDCKE0},
+		{(YSS_GPIO_Peri*)GPIOC, 2, PC2_FMC_SDNE0}
+	};
+
+	gpioA.setPackageAsAltFunc(sdramPort, 39, define::gpio::ospeed::FAST, define::gpio::otype::PUSH_PULL);
+	
+	clock.enableSdram();
+	sdram.init(define::sdram::bank::BANK1, MT48LC16M16A2_6A);
 }
 
